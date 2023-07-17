f317fbb5f90c58d2c9762f4769803301dd2d45ba
yuri-italo <yuri.italo94@gmail.com>
2023-07-17 18:51:22 -0300

    DataIntegrityViolationException added to the exception handler

--- src/main/java/br/com/banco/exception/RestExceptionHandler.java
+++ src/main/java/br/com/banco/exception/RestExceptionHandler.java
@@ -1,6 +1,7 @@
 package br.com.banco.exception;
 
 import br.com.banco.exception.dto.ExceptionDto;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
@@ -20,6 +21,8 @@ public class RestExceptionHandler {
     private static final String METHOD_ARGUMENT_NOT_VALID_EXCEPTION_TITLE = "Validation Error";
     private static final String METHOD_ARGUMENT_TYPE_MISMATCH_EXCEPTION_TITLE = "Error converting value";
     private static final String BUSINESS_EXCEPTION_TITLE = "Business error";
+    private static final String DATA_INTEGRITY_VIOLATION_EXCEPTION_TITLE = "Constraint violation error";
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ExceptionDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         Map<String, String> errors = getBindExceptionErrors(e);
@@ -29,50 +32,33 @@ public class RestExceptionHandler {
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<ExceptionDto> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
-        ExceptionDto exceptionDto = getExceptionDto(e);
+        ExceptionDto exceptionDto = getExceptionDto(e, METHOD_ARGUMENT_TYPE_MISMATCH_EXCEPTION_TITLE, HttpStatus.BAD_REQUEST);
         return new ResponseEntity<>(exceptionDto,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ExceptionDto> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        ExceptionDto exceptionDto = getExceptionDto(e, DATA_INTEGRITY_VIOLATION_EXCEPTION_TITLE, HttpStatus.CONFLICT);
+        return new ResponseEntity<>(exceptionDto,HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ExceptionDto> handleBusinessException(BusinessException e) {
-        ExceptionDto exceptionDto = getBussinessExceptionDto(e);
+        ExceptionDto exceptionDto = getExceptionDto(e, BUSINESS_EXCEPTION_TITLE, HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(exceptionDto,HttpStatus.NOT_FOUND);
     }
 
-    private static ExceptionDto getExceptionDto(Exception e) {
+    private ExceptionDto getExceptionDto(Exception e, String title, HttpStatus status) {
         String name = e.getCause().getClass().getSimpleName();
         String message = Objects.requireNonNull(e.getMessage());
         Map<String, String> errors = Map.of(name, message);
 
         return new ExceptionDto(
-                METHOD_ARGUMENT_TYPE_MISMATCH_EXCEPTION_TITLE,
+                title,
                 LocalDateTime.now(),
-                HttpStatus.BAD_REQUEST.value(),
+                status.value(),
                 e.getClass().getName(),
                 errors);
-    }
-
-    private ExceptionDto getBussinessExceptionDto(BusinessException e) {
-        String name = e.getCause().getClass().getSimpleName();
-        String message = Objects.requireNonNull(e.getMessage());
-        Map<String, String> errors = Map.of(name, message);
-
-        return new ExceptionDto(
-                BUSINESS_EXCEPTION_TITLE,
-                LocalDateTime.now(),
-                HttpStatus.NOT_FOUND.value(),
-                e.getClass().getName(),
-                errors);
-    }
-
-    private Map<String, String> getBindExceptionErrors(BindException e) {
-        Map<String, String> errors = new HashMap<>();
-        e.getBindingResult().getAllErrors().forEach(error -> {
-            String fieldName = ((FieldError) error).getField();
-            String messageError = error.getDefaultMessage();
-            errors.put(fieldName, messageError);
-        });
-        return errors;
     }
 
     private ExceptionDto getBindExceptionDto(BindException e, Map<String, String> errors) {
@@ -83,5 +69,15 @@ public class RestExceptionHandler {
                 e.getClass().getName(),
                 errors
         );
+    }
+
+    private Map<String, String> getBindExceptionErrors(BindException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String messageError = error.getDefaultMessage();
+            errors.put(fieldName, messageError);
+        });
+        return errors;
     }
 }
